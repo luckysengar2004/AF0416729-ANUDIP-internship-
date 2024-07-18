103b49a11c719986eefae08e930a90dff6cdd938
luckysengar2004 <168719671+luckysengar2004@users.noreply.github.com>
2024-07-18 18:05:27 +0530

    Update lect2.py

--- lect2.py
+++ lect2.py
@@ -1,20 +1,22 @@
-'''l=[80,90,30,20,70,80,39,87]
+# Question 1
+# Display the sum and average of all the number present in a list.
+l=[80,90,30,20,70,80,39,87]
 s=0
 for x in l:
     s = s+x
 print("sum is - " , s)
-print("Average is - ",s//len(l))'''
+print("Average is - ",s//len(l)
 
-# question 2
-# display number btw 10 to 500 divisible by 10 and 7
+# Question 2
+# Display number btw 10 to 500 divisible by 10 and 7
 
-'''for i in range(10,500):
+for i in range(10,500):
     if(i % 7 ==0 and i % 10 == 0):
-        print(i)'''
+        print(i)
 
 
-#question3
-# count the number between 100 to 1000 which is even and divisible by 3
+# Question3
+# Count the number between 100 to 1000 which is even and divisible by 3
 print("Number between 100 to 1000 which are even and divisible by 3")
 count = 0
 for i in range(100,1000):
