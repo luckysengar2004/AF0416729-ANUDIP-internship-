4d68b048e6e23b0059576594a6a4dc9b429f2e7f
luckysengar2004 <168719671+luckysengar2004@users.noreply.github.com>
2024-07-23 17:04:51 +0530

    lec7.py -  File handling-Python

--- lec7.py
+++ lec7.py
@@ -1,39 +1,39 @@
 #File handling
 #Creating file
-'''file=open("Lucky.txt",'w')
+file=open("Lucky.txt",'w')
 file.write(" Todays topic is file handling")
-file.close()'''
+file.close()
 
 #Reading file
-'''file=open("Lucky.txt",'r')
+file=open("Lucky.txt",'r')
 print(file.tell()) # It will tell the postion of data in a file
 data=file.read(5)
 print(data)
 print(file.tell())
-file.close()'''
+file.close()
 
 #seek method - change postion of cursor or  file pointer to read data.
-'''file=open("Lucky.txt",'r')
+file=open("Lucky.txt",'r')
 print(file.tell()) 
 file.seek(12)
 data=file.read(5)
 print(data)
 print(file.tell())
-file.close()'''
+file.close()
 
 #Pickle module is used to r/w from binary file. Stores data in same format as in the memory.
-'''import pickle
+import pickle
 file=open("pickle.dat",'wb')
 li=[10,20,30,40,50]
 pickle.dump(li,file)
-file.close()'''
+file.close()
 
 #pickle
-'''import pickle
+import pickle
 file=open("pickle.dat",'rb')
 data=pickle.load(file)
 print(data)
-file.close()'''
+file.close()
 
 #Csv file
 import csv
